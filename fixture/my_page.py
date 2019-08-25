dcc8d42e51f927ccd1b3662a40c4fab4137a1055
betatester25 <betatester2.0@yandex.ru>
2019-08-25 13:10:20 +0300

    Упрощен тест моей страницы

--- fixture/my_page.py
+++ fixture/my_page.py
@@ -10,10 +10,5 @@ class MyPageHelper:
         # переход на мою страницу
         driver.find_element_by_xpath('//span[text()="Моя Страница"]').click()
 
-    def statistic(self):
-        driver = self.method.driver
-        driver.find_element_by_xpath('//div[@onclick="Page.actionsDropdown(domNS(this))"]').click()
-        driver.find_element_by_xpath('//a[text()="Статистика страницы"]').click()
-        # Просмотр посещаемости
-        driver.find_element_by_xpath('(//a[@onclick="return uiTabs.goTab(this, event, 0);"])[2]').click()
+
 
--- test/test_my_page.py
+++ test/test_my_page.py
@@ -2,6 +2,5 @@
 
 def test_assert_of_traffic_my_page(method):
     method.my_page.open()
-    method.my_page.statistic()
-    method.assert_text(word='Уникальные посетители и просмотры')
+
 
