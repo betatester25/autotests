c18589832e5af98015022f2afbbf19dda591f10a
betatester25 <betatester2.0@yandex.ru>
2019-08-29 22:07:18 +0300

    Добавлено два теста для моей страницы

--- fixture/my_page.py
+++ fixture/my_page.py
@@ -10,5 +10,10 @@ class MyPageHelper:
         # переход на мою страницу
         driver.find_element_by_xpath('//span[text()="Моя страница"]').click()
 
+    def open_foto_section(self):
+        driver = self.method.driver
+        driver.find_element_by_xpath('//span[text()="Фотографии"]').click()
 
-
+    def open_my_message(self):
+        driver = self.method.driver
+        driver.find_element_by_xpath('//span[text()="Сообщения"]').click()
--- test/test_my_page.py
+++ test/test_my_page.py
@@ -4,3 +4,9 @@ def test_assert_of_traffic_my_page(method):
     method.my_page.open()
 
 
+def test_view_my_foto(method):
+    method.my_page.open_foto_section()
+
+
+def test_view_my_message(method):
+    method.my_page.open_my_message()
