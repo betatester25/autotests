90d55f043a762780477de651ce44e835dba2081e
betatester25 <betatester2.0@yandex.ru>
2019-08-31 13:37:09 +0300

    ..

--- test/test_my_page.py
+++ test/test_my_page.py
@@ -1,12 +1,10 @@
 import pytest
 
 
-@pytest.allure.step('Просмотр моей страницы')
 def test_view_my_page(method):
     method.my_page.open()
 
 
-@pytest.allure.step('Переход в раздел "Фотографии"')
 def test_view_my_foto(method):
     method.my_page.open_foto_section()
 
